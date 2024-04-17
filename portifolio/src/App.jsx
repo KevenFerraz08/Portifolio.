0680316acd4d538a3ac8e02ae3e3c1f0f9a837d4
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2024-04-17 12:54:42 +0000

    redefinindo

--- portifolio/src/App.jsx
+++ portifolio/src/App.jsx
@@ -2,10 +2,10 @@ import './App.css';
 import APPRoutes from './routes';
 function App() {
   return (
-    <div className="App">
+    <>
       <APPRoutes />
-    </div>
+    </>
   );
 }
 
-export default App;
+export default App; 
--- portifolio/src/components/Section-me/index.jsx
+++ portifolio/src/components/Section-me/index.jsx
@@ -5,19 +5,23 @@ const Section = () => {
         <>
          <section className={style.Section}>
             <div className="container-section">
-                <div className="image">
-                    <center><img src={foto} alt="Foto" className='foto'/></center>
+                <div className="container-image">
+                    <div className="image">
+                        <center><img src={foto} alt="Foto" className='foto'/></center>
+                    </div>
                 </div>
-                <div className="information-section">
-                    <h2>Quem sou</h2>
-                    
-                    <h1>Keven Ferraz de Almeida</h1>
+                <div className="container-informations">
+                    <div className="information-section">
+                        <h2>Quem sou</h2>
                         
-                    <h3>Frontend Developer</h3>
-                        
-                    <p>Meu nome é Keven Ferraz de Almeida, sou dev frontend. Atualmente sou freelancer como Frontend Developer e UI Designer. Desenvolvo interfaces modernas e de alta qualidade, concentrado em performance, animações, responsividade e SEO.</p>
-                    <div className="skills-icon">
+                        <h1>Keven Ferraz de Almeida</h1>
+                            
+                        <h3>Frontend Developer</h3>
+                            
+                        <p>Meu nome é Keven Ferraz de Almeida, sou dev frontend. Atualmente sou freelancer como Frontend Developer e UI Designer. Desenvolvo interfaces modernas e de alta qualidade, concentrado em performance, animações, responsividade e SEO.</p>
+                        <div className="skills-icon">
 
+                        </div>
                     </div>
                 </div>
             </div>
