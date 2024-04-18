23badc5be9a684ee052b018c4554b9982ca5f963
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2024-04-18 14:26:24 +0000

    atualization css

--- portifolio/src/components/Section-me/index.jsx
+++ portifolio/src/components/Section-me/index.jsx
@@ -1,6 +1,11 @@
 import style from './Section.css'
 import foto from '../../assets/img/Keven-Ferraz.png'
-import giticon from'../../assets/img/icon-gith.png'
+import icon_git from '../../assets/icons/github.svg'
+import icon_react from '../../assets/icons/react.svg'
+import icon_js from '../../assets/icons/javascript.svg'
+import icon_css from '../../assets/icons/css.svg'
+import icon_html from '../../assets/icons/html.svg'
+import icon_node from '../../assets/icons/node.svg'
 const Section = () => {
     return(
         <>
@@ -9,7 +14,6 @@ const Section = () => {
                 <div className="container-image">
                     
                     <div className="image">
-                        <p className='text-above-button'>Meu nome é Keven Ferraz de Almeida, sou dev frontend. Atualmente sou freelancer como Frontend Developer e UI Designer. Desenvolvo interfaces modernas e de alta qualidade, concentrado em performance, animações, responsividade e SEO.</p>
                         <center><img src={foto} alt="Foto" className='foto'/></center>
                         <br/>
                         <button className="button">About-me&nbsp;&nbsp;&nbsp;&rarr;</button>
@@ -28,22 +32,22 @@ const Section = () => {
                         Meu nome é Keven Ferraz de Almeida, sou dev frontend. Atualmente sou freelancer como Frontend Developer e UI Designer. Desenvolvo interfaces modernas e de alta qualidade, concentrado em performance, animações, responsividade e SEO.</p>
                         <div className="skills-icons">
                             <div className="icon-skills">
-                               <center> <img align="center" alt="React" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg"/></center>
+                               <center> <img align="center" alt="html" height="30" width="40" src={icon_html}/></center>
                             </div>
                             <div className="icon-skills">
-                                <center><img align="center" alt="C#" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg"/></center>
+                                <center><img align="center" alt="Css" height="30" width="40" src={icon_css}/></center>
                             </div>
                             <div className="icon-skills">
-                               <center> <img align="center" alt="github" height="30" width="40" src={giticon} /></center>
+                               <center> <img align="center" alt="js" height="30" width="40" src={icon_js} /></center>
                             </div>
                             <div className="icon-skills">
-                               <center> <img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"/></center>
+                               <center> <img align="center" alt="Js" height="30" width="40" src={icon_react}/></center>
                             </div>
                             <div className="icon-skills">
-                               <center> <img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"/></center>
+                               <center> <img align="center" alt="node js" height="30" width="40" src={icon_node}/></center>
                             </div>
                             <div className="icon-skills">
-                               <center> <img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"/></center>
+                               <center> <img align="center" alt="github" height="30" width="40" src={icon_git}/></center>
                             </div>
                         </div>
                     </div>
