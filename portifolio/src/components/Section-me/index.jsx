efbf594c04de16f742ef1d187078173233cd900c
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2024-04-18 03:33:54 +0000

    update

--- portifolio/src/components/Section-me/index.jsx
+++ portifolio/src/components/Section-me/index.jsx
@@ -1,12 +1,15 @@
 import style from './Section.css'
 import foto from '../../assets/img/Keven-Ferraz.png'
+import giticon from'../../assets/img/icon-gith.png'
 const Section = () => {
     return(
         <>
          <section className={style.Section}>
             <div className="container-section">
                 <div className="container-image">
+                    
                     <div className="image">
+                        <p className='text-above-button'>Saiba mais sobre mim acessando a pagina About.<br/> basta clicar no botao abaixo</p>
                         <center><img src={foto} alt="Foto" className='foto'/></center>
                         <br/>
                         <button className="button">About-me&nbsp;&nbsp;&nbsp;&rarr;</button>
@@ -21,8 +24,25 @@ const Section = () => {
                         <h3>Frontend Developer</h3>
                             
                         <p>Meu nome é Keven Ferraz de Almeida, sou dev frontend. Atualmente sou freelancer como Frontend Developer e UI Designer. Desenvolvo interfaces modernas e de alta qualidade, concentrado em performance, animações, responsividade e SEO.</p>
-                        <div className="skills-icon">
-
+                        <div className="skills-icons">
+                            <div className="icon-skills">
+                               <center> <img align="center" alt="React" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg"/></center>
+                            </div>
+                            <div className="icon-skills">
+                                <center><img align="center" alt="C#" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg"/></center>
+                            </div>
+                            <div className="icon-skills">
+                               <center> <img align="center" alt="Js" height="30" width="40" src={giticon} /></center>
+                            </div>
+                            <div className="icon-skills">
+                               <center> <img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"/></center>
+                            </div>
+                            <div className="icon-skills">
+                               <center> <img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"/></center>
+                            </div>
+                            <div className="icon-skills">
+                               <center> <img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"/></center>
+                            </div>
                         </div>
                     </div>
                 </div>
