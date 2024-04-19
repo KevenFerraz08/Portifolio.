c7114807aa548cfd943cdc6881fd6a8552a089ef
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2024-04-19 02:19:26 +0000

    alter

--- portifolio/src/components/Header/index.jsx
+++ portifolio/src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
-import Logo from '../../assets/img/LogoTransparente2.png'
-import styles from './Header.css'
+import Logo from '../../assets/img/LogoTransparente2.png';
+import styles from './Header.css';
+import icon_menu from '../../assets/icons/menu.png';
 
 const Header = () => {
     return (
@@ -10,12 +11,23 @@ const Header = () => {
                     <img src={Logo} alt="Logo" />
                 </Link>
                 <ul>
-                    <li><Link to="/" className={styles.NavLink}><span>Home</span></Link></li>
-                    <li><Link to="/Projects" className={styles.NavLink}><span>Projects</span></Link></li>
-                    <li><Link to="/Contact" className={styles.NavLink}><span>Contact</span></Link></li>
-                    <li><Link to="/About" className={styles.NavLink}><span>About</span></Link></li>
-                </ul>
+                    <li>
+                        <Link to="/" className={styles.NavLink}><span>Home</span></Link>
+                    </li>
+                    <li>
+                        <Link to="/Projects" className={styles.NavLink}><span>Projects</span></Link>
+                    </li>
+                    <li>
+                        <Link to="/Contact" className={styles.NavLink}><span>Contact</span></Link>
+                    </li>
+                    <li>
+                        <Link to="/About" className={styles.NavLink}><span>About</span></Link>
+                    </li>
+                </ul> 
             </nav>
+            <div className="nav-icons-container">
+                <img src={icon_menu} alt="menu" className="menu-button"/>
+            </div>
         </header>
     );
 };
