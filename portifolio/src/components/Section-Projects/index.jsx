ed55108974de8d9aed90d836307f8aec000765dc
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2024-04-19 17:31:52 +0000

    ..

--- portifolio/src/components/Section-Projects/index.jsx
+++ portifolio/src/components/Section-Projects/index.jsx
@@ -1,10 +1,25 @@
-import style from './SectionProjects.css' // Corrigido o caminho de importação
-
+import style from './SectionProjects.css' 
+import { Link } from 'react-router-dom'
 const SectionProject = () => {
     return (
         <>
-            <section className={style.SectionProject}> 
-                    
+            <section className={style.SectionProject} id='Section-project'> 
+                <div className="container">
+                    <div className="container-my-work">
+                       <div className="my-work">
+                       <h1>My personal projects and work</h1>
+                        <p>React approaches changes with care. Every React commit is tested on business-critical surfaces with over a billion users. Over 100,000 React components at Meta help validate every migration strategy.
+                        The React team is always researching how to improve React. Some research takes years to pay off. React has a high bar for taking a research idea into production. Only proven approaches become a part of React.</p>
+
+                        <Link className="button-next">Ver mais</Link>
+                       </div>
+                    </div>
+                    <div className="conteiner-project-cards">
+                        <div className="project-card">
+
+                        </div>
+                    </div>
+                </div>
             </section>   
         </>
     )
